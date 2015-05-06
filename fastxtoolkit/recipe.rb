05dadc58071a1c2f2b1e5f6c7ef9f72ce3fb3e16
Jerico Revote <jerico.revote@gmail.com>
2015-05-06 16:46:10 +1000

    Added FPM Cookery recipe for FASTX-Toolkit.

---
+++ fastxtoolkit/recipe.rb
@@ -0,0 +1,19 @@
+class FASTXToolkit < FPM::Cookery::Recipe
+  description 'The FASTX-Toolkit is a collection of command line tools for Short-Reads FASTA/FASTQ files preprocessing.'
+  name 'fastxtoolkit'
+  version '0.0.14' 
+  revision 0
+  homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
+  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/fastx_toolkit-#{version}.tar.bz2"
+  md5 'bf1993c898626bb147de3d6695c20b40'
+  
+
+  def build
+    configure :prefix => prefix
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+  end
+end
