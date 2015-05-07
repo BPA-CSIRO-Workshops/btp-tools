ff543bc63a115eb2bfe75cf6564adc850673f1d9
Jerico Revote <jerico.revote@gmail.com>
2015-05-07 14:31:22 +1000

    Added FPM Cookery recipe for IGV Tools.

---
+++ igvtools/recipe.rb
@@ -0,0 +1,18 @@
+class IGVTools < FPM::Cookery::Recipe
+  description 'The igvtools utility provides a set of tools for pre-processing data files. '
+  name 'igvtoos'
+  version '2.3.40' 
+  revision 0
+  homepage 'https://www.broadinstitute.org/igv/igvtools'
+  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/igvtools_#{version}.zip"
+  md5 '2b8712ef79b2c903f6f3383e20416f72'
+
+  depends ['openjdk-7-jre', 'java7-runtime']
+
+  def build
+  end
+
+  def install
+    bin.install Dir["IGVTools/*"]
+  end
+end
