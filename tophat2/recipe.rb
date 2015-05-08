839f4bf1fa54b9e98f3fdf66c2c2a6fe6f24883c
Jerico Revote <jerico.revote@gmail.com>
2015-05-08 11:58:09 +1000

    Added FPM Cookery recipe for Tophat2.

---
+++ tophat2/recipe.rb
@@ -0,0 +1,18 @@
+class Tophat2 < FPM::Cookery::Recipe
+  description 'Tophat is a fast splice junction mapper for RNA-Seq reads.'
+  name 'tophat2'
+  version '2.0.13' 
+  revision 0
+  homepage 'http://ccb.jhu.edu/software/tophat/index.shtml'
+  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/tophat-#{version}.tar.gz"
+  md5 'df5c22c85d7c631ee37f61e91f81ae4d'
+  
+  def build
+    configure :prefix => prefix
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+  end
+end
