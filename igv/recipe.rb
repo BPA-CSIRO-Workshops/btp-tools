60ff41213822283c8eebf015c69b435ce8a47f2f
Jerico Revote <jerico.revote@gmail.com>
2015-05-27 10:45:51 +1000

    Minor typo.

--- igv/recipe.rb
+++ igv/recipe.rb
@@ -1,7 +1,7 @@
 class IGV < FPM::Cookery::Recipe
   description 'IGV is a high-performance visualization tool for interactive exploration of large, integrated genomic datasets.'
   name 'igv'
-  version '2.3.40' 
+  version '2.3.40'
   revision 0
   homepage 'https://www.broadinstitute.org/igv/'
   source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/IGV_#{version}.zip"
