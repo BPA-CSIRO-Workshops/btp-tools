8ebdcfe5b4b6fefbd6026114114a040493e3599d
Jerico Revote <jerico.revote@gmail.com>
2015-05-31 15:56:58 +1000

    Added FPM Cookery recipe for Skewer.

---
+++ skewer/recipe.rb
@@ -0,0 +1,20 @@
+class Skewer < FPM::Cookery::Recipe
+  description 'A fast and sensitive adapter trimmer for illumina paired-end sequences.'
+  name 'skewer'
+  version '0.0.1' 
+  revision 0
+  homepage 'https://github.com/relipmoc/skewer'
+  source 'https://github.com/relipmoc/skewer.git', :with => :git
+
+  def build
+    inline_replace 'Makefile' do |s|
+      s.gsub! 'LDFLAGS=-pthread -lrt', 'LDFLAGS=-pthread'
+      s.gsub! '$(CXX) $(LDFLAGS) $(OBJECTS) -o $@', '$(CXX) $(LDFLAGS) $(OBJECTS) -o $@ -lrt'
+    end
+    make
+  end
+
+  def install
+    bin.install 'skewer'
+  end
+end
