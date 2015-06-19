8cb94a3e8ee6d26ccab993380cf9caf3988b9ed5
Jerico Revote <jerico.revote@gmail.com>
2015-06-19 11:19:10 +1000

    Added FPM Cookery recipe for gnx-tools.

---
+++ gnx-tools/recipe.rb
@@ -0,0 +1,28 @@
+class GNXTools < FPM::Cookery::Recipe
+  description 'Genome basic statistics tool.'
+  name 'gnx-tools'
+  version '0.0.1'
+  revision 0
+  homepage 'https://github.com/mh11/gnx-tools'
+  source 'https://github.com/mh11/gnx-tools.git', :with => :git
+
+  def build
+    safesystem 'mkdir bin'
+    safesystem 'javac -d bin/ src/uk/ac/ebi/gnx/*'
+    safesystem 'ant -f package.xml'
+
+    File.open('gnx', 'w', 0755) do |f|
+      f.write <<-__EOF
+#!/bin/sh
+
+# Simple wrapper to run gnx-tools
+exec java -jar /usr/share/java/gnx.jar "$@"
+      __EOF
+    end
+  end
+
+  def install
+    share('java').install 'gnx.jar'
+    bin.install 'gnx'
+  end
+end
