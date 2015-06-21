c6a71d3fbab1b76dd54c2dc5a222ccd49405f2e8
Jerico Revote <jerico.revote@gmail.com>
2015-06-21 14:28:37 +1000

    Added igv icon into the builder. Updated recipe to install icon into share directory and setup desktop shortcut for all users.

--- igv/recipe.rb
+++ igv/recipe.rb
@@ -1,7 +1,7 @@
 class IGV < FPM::Cookery::Recipe
   description 'IGV is a high-performance visualization tool for interactive exploration of large, integrated genomic datasets.'
   name 'igv'
-  version '2.3.40'
+  version '2.3.40' 
   revision 0
   homepage 'https://www.broadinstitute.org/igv/'
   source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/IGV_#{version}.zip"
@@ -13,6 +13,25 @@ class IGV < FPM::Cookery::Recipe
   end
 
   def install
+    Dir.chdir "IGV_#{version}" do
+      File.open('IGV.desktop', 'w', 0755) do |f|
+        f.write <<-__EOF
+[Desktop Entry]
+Type=Application
+Name=IGV
+Comment=Interactive Genomics Viewer
+Exec=igv.sh
+Icon=igv_icon
+Terminal=true
+StartupNotify=false
+Categories=Graphics;Science;Math
+        __EOF
+      end
+    end
+    
+    etc('skel/Desktop').mkdir
+    etc('skel/Desktop').install Dir["IGV_#{version}/IGV.desktop"]
+    share('icons/hicolor/48x48/apps/').install workdir('igv_icon.png')
     bin.install Dir["IGV_#{version}/*"]
   end
 end
