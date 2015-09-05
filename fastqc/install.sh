43f76a5eafebc58826aa09750ee065464c4b6004
Jerico Revote <jerico.revote@gmail.com>
2015-09-05 12:25:08 +1000

    Updated FastQC to 0.11.3, adding some sanity checks for the shell based installer.

--- fastqc/install.sh
+++ fastqc/install.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
-install_dir='/tools'
-owner='ubuntu'
-tools_location='https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools'
+install_dir='/usr/local/tools'
+tool_location='http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.3_source.zip'
+tool_name='fastqc'
+md5='80b3ad1af0a2b2594318857ed7d7c237'
+
+function md5check {
+  md5sum -c - <<< "$1 $2"
+  if [ $? -ne 0 ]; then
+    echo "MD5 mismatch on downloaded file, exiting ..."
+    exit
+  fi
+}
 
 ############
 ## FastQC ##
 ############
-tool_name='fastqc'
 if [ ! -e "$install_dir/$tool_name" ]; then
   echo "Creating installation directory for $tool_name"
   mkdir -p "$install_dir/$tool_name"
 else
   echo "Installation directory for $tool_name already exists"
 fi
-# Download the jar files
+
 cd $install_dir/$tool_name
-wget -4 --no-check-certificate $tools_location/fastqc_v0.11.2.zip
-unzip fastqc_v0.11.2.zip
-mv FastQC 0.11.2
-chmod a+x $install_dir/$tool_name/0.11.2/fastqc
-ln -s $install_dir/$tool_name/0.11.2 $install_dir/$tool_name/$tool_name-default
+
+if [ ! -e fastqc_v0.11.3_source.zip ]; then
+  wget -4 --no-check-certificate $tool_location
+fi
+
+md5check $md5 fastqc_v0.11.3_source.zip
+unzip fastqc_v0.11.3_source.zip
+mv FastQC default
+chmod a+x $install_dir/$tool_name/default/fastqc
+
 # Cleanup
-rm fastqc_v0.11.2.zip
+rm fastqc_v0.11.3_source.zip
 ####################
 
 #################
 ## Setup Paths ##
 #################
-chown -R $owner.$owner $install_dir/$tool_name
-echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/$tool_name-default ; then" > /etc/profile.d/$tool_name.sh
-echo "PATH=$install_dir/$tool_name/$tool_name-default:\${PATH}" >> /etc/profile.d/$tool_name.sh
+chown -R root.root $install_dir/$tool_name
+echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/default ; then" > /etc/profile.d/$tool_name.sh
+echo "PATH=$install_dir/$tool_name/default:\${PATH}" >> /etc/profile.d/$tool_name.sh
 echo "fi" >> /etc/profile.d/$tool_name.sh
--- fastqc/recipe.rb
+++ fastqc/recipe.rb
@@ -1,11 +1,11 @@
 class FastQC < FPM::Cookery::Recipe
   description 'FastQC is a quality control tool for high throughput sequence data.'
   name 'fastqc'
-  version '0.11.2' 
+  version '0.11.3' 
   revision 0
   homepage 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/'
-  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/fastqc_v#{version}.zip"
-  md5 '8686e180a2d7c818821eb7db194d9488'
+  source "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v#{version}_source.zip"
+  md5 '80b3ad1af0a2b2594318857ed7d7c237'
 
   def build
   end
