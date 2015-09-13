1df359072d920a8c588a76f40f2e2cd53069e640
Jerico Revote <jerico.revote@gmail.com>
2015-09-13 13:05:16 +1000

    Rejigged shell installer for picard, added dependencies.

--- picard/install.sh
+++ picard/install.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
-install_dir='/tools'
-owner='ubuntu'
-tools_location='https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools'
+install_dir='/usr/local/tools'
+tool_location='https://github.com/broadinstitute/picard/releases/download/1.138/picard-tools-1.138.zip'
+tool_name='picard'
+md5='0f71cb71c90393e501b8ebc9f867dc0b'
+
+function md5check {
+  md5sum -c - <<< "$1 $2"
+  if [ $? -ne 0 ]; then
+    echo "MD5 mismatch on downloaded file, exiting ..."
+    exit
+  fi
+}
+
+##################
+## Dependencies ##
+##################
+dependencies=(default-jre default-jdk ant unzip)
+apt-get update
+apt-get install -y ${dependencies[@]}
+apt-get clean
 
 ############
 ## Picard ##
 ############
-tool_name='picard'
 if [ ! -e "$install_dir/$tool_name" ]; then
   echo "Creating installation directory for $tool_name"
   mkdir -p "$install_dir/$tool_name"
 else
   echo "Installation directory for $tool_name already exists"
 fi
-# Download the jar files
+
 cd $install_dir/$tool_name
-wget -4 --no-check-certificate $tools_location/picard-tools-1.119.zip
-unzip picard-tools-1.119.zip
-mv picard-tools-1.119 1.119
-ln -s $install_dir/$tool_name/1.119 $install_dir/$tool_name/$tool_name-default
+
+if [ ! -f picard-tools-1.138.zip ]; then
+  wget -4 --no-check-certificate $tool_location
+fi
+
+md5check $md5 picard-tools-1.138.zip
+unzip picard-tools-1.138.zip
+mv picard-tools-1.138 default
+
 # Cleanup
-rm picard-tools-1.119.zip
+rm picard-tools-1.138.zip
 ####################
 
 #################
 ## Setup Paths ##
 #################
 chown -R $owner.$owner $install_dir/$tool_name
-echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/$tool_name-default ; then" > /etc/profile.d/$tool_name.sh
-echo "PATH=$install_dir/$tool_name/$tool_name-default:\${PATH}" >> /etc/profile.d/$tool_name.sh
+echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/default ; then" > /etc/profile.d/$tool_name.sh
+echo "PATH=$install_dir/$tool_name/default:\${PATH}" >> /etc/profile.d/$tool_name.sh
 echo "fi" >> /etc/profile.d/$tool_name.sh
--- picard/recipe.rb
+++ picard/recipe.rb
@@ -1,13 +1,14 @@
 class Picard < FPM::Cookery::Recipe
   description 'A set of tools (in Java) for working with next generation sequencing data in the BAM (http://samtools.sourceforge.net) format.'
   name 'picard'
-  version '1.119' 
+  version '1.138' 
   revision 0
   homepage 'http://broadinstitute.github.io/picard/'
   source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/picard-tools-#{version}.zip"
-  md5 '84aee7dcce1dc61724e8bba3eb3cd8cc'
+  source "https://github.com/broadinstitute/picard/releases/download/#{version}/picard-tools-#{version}.zip"
+  md5 '0f71cb71c90393e501b8ebc9f867dc0b'
 
-  build_depends ['curl']
+  build_depends ['curl', 'default-jre', 'default-jdk', 'ant']
 
   def build
   end
