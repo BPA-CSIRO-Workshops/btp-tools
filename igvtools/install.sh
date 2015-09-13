7f04a9183ed73cefc012ea6de4629b5e10e6e801
Jerico Revote <jerico.revote@gmail.com>
2015-09-13 10:53:26 +1000

    Updated shell script installer for igvtools, added required dependencies.

--- igvtools/install.sh
+++ igvtools/install.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
-install_dir='/tools'
-owner='ubuntu'
-tools_location='https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools'
+install_dir='/usr/local/tools'
+tool_location='http://data.broadinstitute.org/igv/projects/downloads/igvtools_2.3.57.zip'
+tool_name='igvtools'
+md5='68a3230698bd530544ced92414d45c33'
+
+function md5check {
+  md5sum -c - <<< "$1 $2"
+  if [ $? -ne 0 ]; then
+    echo "MD5 mismatch on downloaded file, exiting ..."
+    exit
+  fi
+}
+
+##################
+## Dependencies ##
+##################
+dependencies=(default-jre default-jdk unzip)
+apt-get update
+apt-get install -y ${dependencies[@]}
+apt-get clean
 
 ##############
 ## IGVTools ##
 ##############
-tool_name='igvtools'
 if [ ! -e "$install_dir/$tool_name" ]; then
   echo "Creating installation directory for $tool_name"
   mkdir -p "$install_dir/$tool_name"
 else
   echo "Installation directory for $tool_name already exists"
 fi
-# Download the jar files
+
 cd $install_dir/$tool_name
-wget -4 --no-check-certificate $tools_location/igvtools_2.3.40.zip
-unzip igvtools_2.3.40.zip
-ln -s $install_dir/$tool_name/2.3.40 $install_dir/$tool_name/$tool_name-default
-mv IGVTools 2.3.40
-# Cleanup
-rm igvtools_2.3.40.zip
+
+if [ ! -f igvtools_2.3.57.zip ]; then
+  wget -4 --no-check-certificate $tool_location
+fi
+
+unzip igvtools_2.3.57.zip
+mv IGVTools default
 ####################
 
 #################
 ## Setup Paths ##
 #################
 chown -R $owner.$owner $install_dir/$tool_name
-echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/$tool_name-default ; then" > /etc/profile.d/$tool_name.sh
-echo "PATH=$install_dir/$tool_name/$tool_name-default:\${PATH}" >> /etc/profile.d/$tool_name.sh
+echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/default ; then" > /etc/profile.d/$tool_name.sh
+echo "PATH=$install_dir/$tool_name/default:\${PATH}" >> /etc/profile.d/$tool_name.sh
 echo "fi" >> /etc/profile.d/$tool_name.sh
--- igvtools/recipe.rb
+++ igvtools/recipe.rb
@@ -1,13 +1,13 @@
 class IGVTools < FPM::Cookery::Recipe
   description 'The igvtools utility provides a set of tools for pre-processing data files. '
   name 'igvtools'
-  version '2.3.40' 
+  version '2.3.57' 
   revision 0
   homepage 'https://www.broadinstitute.org/igv/igvtools'
-  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/igvtools_#{version}.zip"
-  md5 '2b8712ef79b2c903f6f3383e20416f72'
+  source "http://data.broadinstitute.org/igv/projects/downloads/igvtools_#{version}.zip"
+  md5 '68a3230698bd530544ced92414d45c33'
 
-  depends ['curl', 'openjdk-7-jre', 'java7-runtime']
+  depends ['curl', 'default-jre', 'default-jdk']
 
   def build
   end
