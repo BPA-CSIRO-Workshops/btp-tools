be695c5586d33b5b39724e7fdebd1a7df073848a
Jerico Revote <jerico.revote@gmail.com>
2015-09-18 13:06:51 +1000

    Rejigged velvet, added dependencies to be installed for shell/fpm builders.

--- velvet/install.sh
+++ velvet/install.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-install_dir='/tools'
-owner='ubuntu'
-tools_location='https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools'
+install_dir='/usr/local/tools'
+tool_location='https://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz'
+tool_name='velvet'
+md5='6e28c4b9bedc5f7ab2b947e7266a02f6'
+
+function md5check {
+  md5sum -c - <<< "$1 $2"
+  if [ $? -ne 0 ]; then
+    echo "MD5 mismatch on downloaded file, exiting ..."
+    exit
+  fi
+}
+
+##################
+## Dependencies ##
+##################
+dependencies=(wget zlib1g-dev)
+apt-get update
+apt-get install -y ${dependencies[@]}
+apt-get clean
 
 ############
 ## Velvet ##
 ############
-tool_name='velvet'
 if [ ! -e "$install_dir/$tool_name" ]; then
   echo "Creating installation directory for $tool_name"
   mkdir -p "$install_dir/$tool_name"
 else
   echo "Installation directory for $tool_name already exists"
 fi
-# Download the source code
+
 cd $install_dir/$tool_name
-wget -4 --no-check-certificate $tools_location/velvet_1.2.10.tgz
-tar -xzf velvet_1.2.10.tgz
+
+if [ ! -f velvet_1.2.10.tgz ]; then
+  wget -4 --no-check-certificate $tool_location
+fi
+
 # Compile
-mv velvet_1.2.10 1.2.10
-cd 1.2.10
+tar -xzf velvet_1.2.10.tgz
+mv velvet_1.2.10 default
+cd default
 make 'MAXKMERLENGTH=59' 'LONGSEQUENCES=1' 'OPENMP=1'
 make 'MAXKMERLENGTH=59' 'LONGSEQUENCES=1' 'OPENMP=1' color
 make doc
-ln -s $install_dir/$tool_name/1.2.10 $install_dir/$tool_name/$tool_name-default
+
 # Cleanup
 cd ../
 rm velvet_1.2.10.tgz
@@ -33,9 +53,9 @@ rm velvet_1.2.10.tgz
 #################
 ## Setup Paths ##
 #################
-chown -R $owner.$owner $install_dir/$tool_name
-echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/$tool_name-default ; then" > /etc/profile.d/$tool_name.sh
-echo "PATH=$install_dir/$tool_name/$tool_name-default:\${PATH}" >> /etc/profile.d/$tool_name.sh
+chown -R root.root $install_dir/$tool_name
+echo "if ! echo \${PATH} | /bin/grep -q $install_dir/$tool_name/default ; then" > /etc/profile.d/$tool_name.sh
+echo "PATH=$install_dir/$tool_name/default:\${PATH}" >> /etc/profile.d/$tool_name.sh
 echo "fi" >> /etc/profile.d/$tool_name.sh
-echo "alias velveth_long=$install_dir/$tool_name/$tool_name-default/velveth" >> /etc/profile.d/$tool_name.sh
-echo "alias velvetg_long=$install_dir/$tool_name/$tool_name-default/velvetg" >> /etc/profile.d/$tool_name.sh
+echo "alias velveth_long=$install_dir/$tool_name/default/velveth" >> /etc/profile.d/$tool_name.sh
+echo "alias velvetg_long=$install_dir/$tool_name/default/velvetg" >> /etc/profile.d/$tool_name.sh
--- velvet/recipe.rb
+++ velvet/recipe.rb
@@ -4,10 +4,10 @@ class Velvet < FPM::Cookery::Recipe
   version '1.2.10'
   revision 0
   homepage 'https://www.ebi.ac.uk/~zerbino/velvet/'
-  source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/velvet_#{version}.tgz"
+  source "https://www.ebi.ac.uk/~zerbino/velvet/velvet_#{version}.tgz"
   md5 '6e28c4b9bedc5f7ab2b947e7266a02f6'
 
-  build_depends ['curl']
+  build_depends ['curl', 'zlib1g-dev']
 
   def build
     safesystem "make 'MAXKMERLENGTH=59' 'LONGSEQUENCES=1' 'OPENMP=1'"
