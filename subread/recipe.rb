2e6e172764b3f29b38464094321d4f1e040ac7ba
Jerico Revote <jerico.revote@gmail.com>
2015-09-30 15:02:10 +1000

    Just adding a small comment for deps install.

--- subread/recipe.rb
+++ subread/recipe.rb
@@ -7,6 +7,7 @@ class Subread < FPM::Cookery::Recipe
   source "http://sourceforge.net/projects/subread/files/subread-1.4.6-p4/subread-#{version}-source.tar.gz"
   md5 '2ad116fe3e76c9d4f9f0481419681e20'
 
+  # Let's install build dependencies first.
   build_depends ['make', 'gcc', 'zlib1g', 'zlib1g-dev', 'wget']
 
   def build
