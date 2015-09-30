adb2082a51e64b9a27f3d0485a6b830bb11f6020
Jerico Revote <jerico.revote@gmail.com>
2015-09-30 11:03:55 +1000

    Added simple comment for installing dependencies.

--- blat/recipe.rb
+++ blat/recipe.rb
@@ -7,6 +7,7 @@ class Blat < FPM::Cookery::Recipe
   source "https://swift.rc.nectar.org.au:8888/v1/AUTH_809/Tools/blatSrc35.zip"
   md5 '16e546b8843b85e0b0f2fa603cd78724'
 
+  # Let's install build dependencies first.
   build_depends ['curl', 'libpng12-dev']
 
   def build
