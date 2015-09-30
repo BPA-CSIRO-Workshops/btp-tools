094822a9cb9d00a56bea8b91548e432a7dbf7e45
Jerico Revote <jerico.revote@gmail.com>
2015-09-30 13:54:57 +1000

    Added java as a install dep for the tool.

--- picard/recipe.rb
+++ picard/recipe.rb
@@ -8,7 +8,9 @@ class Picard < FPM::Cookery::Recipe
   source "https://github.com/broadinstitute/picard/releases/download/#{version}/picard-tools-#{version}.zip"
   md5 '0f71cb71c90393e501b8ebc9f867dc0b'
 
+  # Let's install build dependencies first.
   build_depends ['curl', 'default-jre', 'default-jdk', 'ant']
+  depends ['default-jre', 'default-jdk']
 
   def build
   end
