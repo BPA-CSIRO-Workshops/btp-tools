e849c239296f2fdbc7c682e409fa4a8f19031546
Jerico Revote <jerico.revote@gmail.com>
2015-10-04 11:46:04 +1100

    Dummy commit.

--- samtools/recipe.rb
+++ samtools/recipe.rb
@@ -7,10 +7,10 @@ class SAMtools < FPM::Cookery::Recipe
   source "http://sourceforge.net/projects/samtools/files/samtools/#{version}/samtools-#{version}.tar.bz2"
   md5 '71dab132e21c0766f0de84c2371a9157'
 
-  # Let's install build dependencies first.
+  # Let's install build dependencies first:
   build_depends ['curl', 'zlib1g-dev', 'ncurses-dev']
 
-  # Build
+  # Build:
   def build
     make
   end
