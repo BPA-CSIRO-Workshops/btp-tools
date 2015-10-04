5330adedb160140b70c8c933826a706fffca678a
Jerico Revote <jerico.revote@gmail.com>
2015-10-04 12:42:20 +1100

    Test amos.

--- amos/recipe.rb
+++ amos/recipe.rb
@@ -7,8 +7,8 @@ class AMOS < FPM::Cookery::Recipe
   source "http://sourceforge.net/projects/amos/files/amos/#{version}/amos-#{version}.tar.gz"
   md5 '51ebe835c93eef9acd39101738b2048c'
   
-  # Let's install build dependencies first.
-  build_depends ['curl', 'make', 'gcc', 'g++', 'libboost-all-dev', 'libqt4-dev', 'libxml-perl', 'libdbi-perl', 'libstatistics-descriptive-perl']
+  # Let's install build dependencies first:
+  build_depends ['curl', 'make', 'g++', 'libboost-all-dev', 'libqt4-dev', 'libxml-perl', 'libdbi-perl', 'libstatistics-descriptive-perl']
 
   # Build:
   def build
