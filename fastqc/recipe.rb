3bc6f076b89d93e9df1c0fd17ace5f5f099ebaad
Jerico Revote <jerico.revote@gmail.com>
2016-06-06 15:32:04 +1000

    Update FastQC to latest version (0.11.5).

--- fastqc/recipe.rb
+++ fastqc/recipe.rb
@@ -1,11 +1,12 @@
 class FastQC < FPM::Cookery::Recipe
   description 'FastQC is a quality control tool for high throughput sequence data.'
   name 'fastqc'
-  version '0.11.3' 
+  version '0.11.5' 
   revision 0
   homepage 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/'
   source "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v#{version}_source.zip"
-  md5 '80b3ad1af0a2b2594318857ed7d7c237'
+  source "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v#{version}_source.zip"
+  md5 '45fe3d34b5a1be285f70fd7de6a8d85f'
 
   # Let's install build dependencies first:
   build_depends ['curl', 'g++', 'unzip', 'wget']
