601a1ac46109633fb087b190b4069bbeb07a84a9
Jerico Revote <jerico.revote@gmail.com>
2016-06-08 10:00:25 +0000

    Simply download bowtie2 binary and package it up.

--- bowtie2/recipe.rb
+++ bowtie2/recipe.rb
@@ -4,23 +4,18 @@ class Bowtie2 < FPM::Cookery::Recipe
   version '2.2.4'
   revision 0
   homepage 'http://bowtie-bio.sourceforge.net/index.shtml'
-  source "http://sourceforge.net/projects/bowtie-bio/files/bowtie2/#{version}/bowtie2-#{version}-source.zip"
-  md5 'd31294875092e76ebb061eadc7998582'
+  source "https://sourceforge.net/projects/bowtie-bio/files/bowtie2/#{version}/bowtie2-#{version}-linux-x86_64.zip"
+  md5 'e54e41c720121da9f234319359347bab'
 
   # Let's install build dependencies first.
-  build_depends ['curl', 'g++', 'unzip', 'wget']
+  build_depends ['curl', 'unzip', 'wget']
 
   # Build:
   def build
-    Dir.chdir "bowtie2-#{version}" do
-      make
-    end
   end
 
   # Install:
   def install
-    Dir.chdir "bowtie2-#{version}" do
-      bin.install ['bowtie2', 'bowtie2-align-s', 'bowtie2-align-l', 'bowtie2-build', 'bowtie2-build-s', 'bowtie2-build-l', 'bowtie2-inspect', 'bowtie2-inspect-s', 'bowtie2-inspect-l']
-    end
+    bin.install Dir["bowtie2-#{version}/bowtie*"]
   end
 end
