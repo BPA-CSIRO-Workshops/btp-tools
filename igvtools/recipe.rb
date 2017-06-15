f659d4dd706d16bf35936b00a47af187bc71dd28
Jerico Revote <jerico.revote@gmail.com>
2017-06-15 04:32:46 +0000

    Update igv version to 3.7.94.

--- igvtools/recipe.rb
+++ igvtools/recipe.rb
@@ -1,11 +1,11 @@
 class IGVTools < FPM::Cookery::Recipe
   description 'The igvtools utility provides a set of tools for pre-processing data files. '
   name 'igvtools'
-  version '2.3.75' 
+  version '2.3.94' 
   revision 0
   homepage 'https://www.broadinstitute.org/igv/igvtools'
   source "http://data.broadinstitute.org/igv/projects/downloads/igvtools_#{version}.zip"
-  md5 '84fe108e4b77f2a2a213c9e32fde779e'
+  md5 'b7faea83f121246a1e9bf03f26ba52ee'
 
   # Let's install build dependencies first.
   depends ['default-jre', 'default-jdk']
