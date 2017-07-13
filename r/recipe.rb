780285329b35a3f4a29b277b93fb8e0d2269dabc
Jerico Revote <jerico.revote@gmail.com>
2017-07-13 04:21:23 +0000

    Upgrade R version to 3.4.0.

--- r/recipe.rb
+++ r/recipe.rb
@@ -1,11 +1,11 @@
 class RBase < FPM::Cookery::Recipe
   description 'R is a free software environment for statistical computing and graphics.'
   name 'rbase'
-  version '3.3.0' 
+  version '3.4.0' 
   revision 0
   homepage 'http://www.r-project.org/'
   source "https://cran.r-project.org/src/base/R-3/R-#{version}.tar.gz"
-  md5 '5a7506c8813432d1621c9725e86baf7a'
+  md5 '75083c23d507b9c16d5c6afbd7a827e7'
 
   # Let's install build dependencies first.
   build_depends ['curl', 'gcc', 'g++', 'gfortran', 'bison', 'debhelper', 'dh-apparmor', 'gir1.2-freedesktop', 'gir1.2-pango-1.0', 'html2text', 'libbison-dev', 'libbz2-dev', 'libcairo-script-interpreter2', 'libcairo2-dev', 'libglib2.0-bin', 'libglib2.0-data', 'libglib2.0-dev', 'libjpeg-dev', 'libjpeg-turbo8-dev', 'libjpeg8-dev', 'liblzma-dev', 'libpango1.0-dev', 'libpcre3-dev', 'libpcrecpp0', 'libreadline-dev', 'libxcb-render0-dev', 'libxcb-shm0-dev', 'tcl8.5', 'tcl8.5-dev', 'texi2html', 'tk8.5', 'tk8.5-dev', 'libx11-dev', 'x11-xkb-utils', 'libxt-dev', 'xdg-utils', 'xfonts-base', 'xserver-common', 'xvfb', 'default-jre', 'default-jdk', 'libxml2-dev', 'libcurl4-openssl-dev']
@@ -29,6 +29,11 @@ biocLite("RColorBrewer")
 biocLite("GO.db")
 biocLite("biomaRt")
 biocLite("org.Hs.eg.db")
+biocLite("SomaticSignatures")
+local({r <- getOption("repos")
+       r["CRAN"] <- "https://cran.ms.unimelb.edu.au"
+       options(repos=r)})
+install.packages("sequenza")
       __EOF
     end
 
